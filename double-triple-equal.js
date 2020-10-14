1c6702248774502a22c9449ea565e95b6ac86565
tonmoy <tonmoy0199322@gmail.com>
2020-10-14 14:11:16 +0600

    double-triple-equal

---
+++ double-triple-equal.js
@@ -0,0 +1,8 @@
+const first = 23;
+const second = '23';
+if (first === second) {           // double == means not check type of value
+                                   // triple === means check type of value (strickly check)
+    console.log('conditon is true')
+} else {
+    console.log('conditon is false')
+}
---
+++ true-false.js
@@ -0,0 +1,7 @@
+const age = 10;
+if (age > 9) {
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
